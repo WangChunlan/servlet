60c42b839f504809c3b8589454cb93b9e78a582c
wangchunlan <jinlan10955@gmail.com>
2019-04-03 17:42:42 +0800

    搭建javaEE  -  Web-Application

---
+++ src/servlet/miniServlet.java
@@ -0,0 +1,13 @@
+package servlet;
+
+/**
+ * @author wangchunlan
+ * @ClassName miniServlet.java
+ * @Description
+ * @createTime 2019年04月03日 17:39:00
+ */
+public class miniServlet {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
